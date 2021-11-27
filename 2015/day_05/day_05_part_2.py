2be06fdde8234a2d3fa6f9aed16aa900f8f7fb3b
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-11-27 12:36:23 +0100

    Ajout 2015/05 part 2

--- 2015/day_05/day_05_part_2.py
+++ 2015/day_05/day_05_part_2.py
@@ -1,2 +1,18 @@
+import re
+import string
+
 with open('2015/day_05/list.txt', encoding="utf-8") as f:
     lines = f.readlines()
+
+t = list(string.ascii_lowercase)
+
+tab = []
+for i in range(len(lines)):
+    for a in range(len(t)):
+        if re.search(t[a]+'[^.]'+t[a]+']*', lines[i]):
+            for b in range(len(t)):
+                for c in range(len(t)):
+                    if len(re.findall(t[b]+t[c], lines[i])) >= 2:
+                        tab.append(lines[i])
+
+print(len(set(tab)))
