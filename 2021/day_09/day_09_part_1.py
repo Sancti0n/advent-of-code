b4df9500561cdf580e11aff9f037eeb98f0fd42f
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-12-12 23:13:05 +0100

    Update 2021/09 part 1

--- 2021/day_09/day_09_part_1.py
+++ 2021/day_09/day_09_part_1.py
@@ -4,33 +4,40 @@ with open('2021/day_09/list.txt', encoding="utf-8") as f:
 for i in range(len(lines)):
     lines[i] = lines[i].split('\n')[0]
 
+def comparaison(a,b,c,d):
+    if lines[a][b]<lines[c][d]: return True
+    else: return False
+
+def addV(i,a,t):
+    t.append(lines[i][a])
+
 t = []
 for i in range(len(lines)):
     for a in range(len(lines[i])):
         if i>0 and i<len(lines)-1:
             if a+1<len(lines[i]):
                 if a>0:
-                    if lines[i][a]<lines[i][a+1] and lines[i][a-1]>lines[i][a] and lines[i-1][a]>lines[i][a] and lines[i+1][a]>lines[i][a]: t.append(lines[i][a])
+                    if comparaison(i,a,i,a+1) and comparaison(i,a,i,a-1) and comparaison(i,a,i-1,a) and comparaison(i,a,i+1,a): addV(i,a,t)
                 if a==0:
-                    if lines[i][a]<lines[i][a+1] and lines[i][a]<lines[i-1][a] and lines[i][a]<lines[i+1][a]: t.append(lines[i][a])
+                    if comparaison(i,a,i,a+1) and comparaison(i,a,i-1,a) and comparaison(i,a,i+1,a): addV(i,a,t)
             if a+1==len(lines[i]):
-                if lines[i][a]<lines[i][a-1] and lines[i][a]<lines[i-1][a] and lines[i][a]<lines[i+1][a]: t.append(lines[i][a])
+                if comparaison(i,a,i,a-1) and comparaison(i,a,i-1,a) and comparaison(i,a,i+1,a): addV(i,a,t)
         if i==0:
             if a+1<len(lines[i]):
                 if a>0:
-                    if lines[i][a]<lines[i][a+1] and lines[i][a]<lines[i][a-1] and lines[i][a]<lines[i+1][a]: t.append(lines[i][a])
+                    if comparaison(i,a,i,a+1) and comparaison(i,a,i,a-1) and comparaison(i,a,i+1,a): addV(i,a,t)
                 if a==0:
-                    if lines[i][a]<lines[i][a+1] and lines[i][a]<lines[i+1][a]: t.append(lines[i][a])
+                    if comparaison(i,a,i,a+1) and comparaison(i,a,i+1,a): addV(i,a,t)
             if a+1==len(lines[i]):
-                if lines[i][a]<lines[i][a-1] and lines[i][a]<lines[i+1][a]: t.append(lines[i][a])
+                if comparaison(i,a,i,a-1) and comparaison(i,a,i+1,a): addV(i,a,t)
         if i==len(lines)-1:
             if a+1<len(lines[i]):
                 if a>0:
-                    if lines[i][a]<lines[i][a+1] and lines[i][a]<lines[i][a-1] and lines[i][a]<lines[i-1][a]: t.append(lines[i][a])
+                    if comparaison(i,a,i,a+1) and comparaison(i,a,i,a-1) and comparaison(i,a,i-1,a): addV(i,a,t)
                 if a==0:
-                    if lines[i][a]<lines[i][a+1] and lines[i][a]<lines[i-1][a]: t.append(lines[i][a])
+                    if comparaison(i,a,i,a+1) and comparaison(i,a,i-1,a): addV(i,a,t)
             if a+1==len(lines[i]):
-                if lines[i][a]<lines[i][a-1] and lines[i][a]<lines[i-1][a]: t.append(lines[i][a])
+                if comparaison(i,a,i,a-1) and comparaison(i,a,i-1,a): addV(i,a,t)
 
 count = 0
 for nb in t:
