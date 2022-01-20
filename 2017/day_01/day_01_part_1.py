d853d21e5154ce4fb99fb48a5b115246f5f1ef5b
Jeremie Maufret <sayou.jeremie@gmail.com>
2022-01-20 21:34:31 +0100

    Ajout 2017/01 part 1

---
+++ 2017/day_01/day_01_part_1.py
@@ -0,0 +1,11 @@
+with open('2017/day_01/list.txt', encoding="utf-8") as f:
+    lines = f.readlines()
+
+v = 0
+for i in range(len(lines[0])):
+    if i == 0: 
+        if lines[0][0] == lines[0][len(lines[0])-1]: v += int(lines[0][0])
+    else:
+        if lines[0][i] == lines[0][i-1]: v += int(lines[0][i])
+print(v)
+        
