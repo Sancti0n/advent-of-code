a1f2826236fc896a5743107a94a1ff6df87a928e
Jeremie Maufret <sayou.jeremie@gmail.com>
2022-02-03 15:01:40 +0100

    Ajout 2015/10 part 1 & 2

---
+++ 2015/day_10/day_10_part_2.py
@@ -0,0 +1,23 @@
+input_ = '1321131112'
+
+def number(nb, time):
+    a = 0
+    while a<time:
+        for i in range(len(nb)):
+            if i == 0:
+                w = ''
+                v = nb[i]
+                c = 0
+            if nb[i] == v:
+                c += 1
+            if nb[i] != v:
+                w += str(c) + v
+                v = nb[i]
+                c = 1
+            if i == len(nb)-1:
+                w += str(c) + v
+        nb = w
+        a += 1
+    return w
+print(len(number(input_, 50)))
+
